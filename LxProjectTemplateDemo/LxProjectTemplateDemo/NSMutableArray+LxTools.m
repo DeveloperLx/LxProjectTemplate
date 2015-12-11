67be9efcbff5277dea4dc9bd650e4bd33969f696
DeveloperLx <lixian@etiantian.com>
2015-12-11 14:04:52 +0800

    mutablearray category finished

--- LxProjectTemplateDemo/LxProjectTemplateDemo/NSMutableArray+LxTools.m
+++ LxProjectTemplateDemo/LxProjectTemplateDemo/NSMutableArray+LxTools.m
@@ -10,4 +10,19 @@
 
 @implementation NSMutableArray (LxTools)
 
+- (void)disorder
+{
+    [self sortUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        
+        return (NSComparisonResult)(arc4random()%3 - 1);
+    }];
+}
+
+- (void)reverse
+{
+    for (int i = 0; i < self.count/2; i++) {
+        [self exchangeObjectAtIndex:i withObjectAtIndex:self.count - 1 - i];
+    }
+}
+
 @end
